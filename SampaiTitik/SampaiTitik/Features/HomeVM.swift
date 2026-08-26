aa30d34cb9b8502489536101753fb4b1fa3a2cb0
Salman <127771549+salmanlfz@users.noreply.github.com>
2026-08-26 22:58:08 +0700

    E/18 enhancement homepage (#21)
    * feat:
    
    * feat: add stationSelection card button and the logic

---
+++ SampaiTitik/SampaiTitik/Features/HomeVM.swift
@@ -0,0 +1,23 @@
+//
+//  StationSelectionViewModel.swift
+//  SampaiTitik
+//
+//  Created by Salman on 24/08/26.
+//
+
+import Combine
+import Observation
+
+@Observable
+class HomeViewModel {
+    var departStation: String = ""
+    var destStation: String = ""
+    var showDeparture: Bool = false
+    var showDestination: Bool = false
+    var isRotating: Bool = false
+    
+    func swapStations(){
+        (departStation, destStation) = (destStation, departStation)
+        isRotating.toggle()
+    }
+}
